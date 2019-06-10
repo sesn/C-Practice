d44c6bebca8fb5b610f8c664bdc95afe7daa2b4f
sesn <sesankar11@gmail.com>
2019-06-10 23:54:02 +0530

    excerise-11

---
+++ excerises/chap-1/wordcount.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main() {
+    int c, nl, nw, nc, state;
+    
+    nl = nw = nc = 0;
+    state = OUT;
+    
+    while((c = getchar()) != EOF) {
+        ++nc;
+        k
+    }
+}
